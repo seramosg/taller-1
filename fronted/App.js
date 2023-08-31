142950845c37bca75afbc1982c46a974f452ccdd
jergonzalezco <jergonzalezco@unal.edu.co>
2023-08-31 10:56:52 -0500

    Se utilizó ciertas herramientas para diseñar un pequeño frontend mediante ReactNative

--- fronted/App.js
+++ fronted/App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar'; <StatusBar style="auto" />
+import { StyleSheet, Text, View, Button, Image, TextInput, FlatList, useState } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>TALLER CALIFICABLE 1</Text>
+      <Text style={styles.letra}>5 razones para ver la asignatura:</Text>
+      <FlatList
+        data={[
+          {key: 'Conocimientos nuevos'},
+          {key: 'Oportunidades en el campo laboral'},
+          {key: 'Complicada pero trabajable'},
+          {key: 'Demasiado aprendizaje'},
+          {key: 'El profe es el mejor'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+      <TextInput placeholder='Ingrese su cédula: ' style={styles.input} />
+      <Button color="green" title="Haz click" />
+      <Image source={{uri: 'https://www.eluniverso.com/resizer/IX818Ewt6Sf5nUstH_ULvy0IopQ=/1001x670/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/5WYTZSATWVEUBJHSHXLOACTULI.jpg'}} style={styles.imagen} />
     </View>
+    
+
   );
 }
 
@@ -15,6 +30,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titulo: {
+    flex: 1,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  letra: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    backgroundColor: '#2fbf',
+    borderRadius: 15,
+  },
+  input: {
+    marginTop: 10,
+    fontSize: 20,
+    color: 'blue',
+  },
+  imagen: {
+    width: 500,
+    height: 500,
+  },
+  item: {
+    fontSize: 18,
+    height: 25,
   },
 });
